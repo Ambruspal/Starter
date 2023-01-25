f28731bc17ff11b988ba9805161d4626cd80c6a7
Ambruspal <phaal83@gmail.com>
2023-01-25 15:57:18 +0100

    Mentés flat fájlba működik

--- src/main/java/hu/lechnerkozpont/ambruspal/vehicle/starter/Main.java
+++ src/main/java/hu/lechnerkozpont/ambruspal/vehicle/starter/Main.java
@@ -2,13 +2,10 @@ package hu.lechnerkozpont.ambruspal.vehicle.starter;
 
 import hu.lechnerkozpont.ambruspal.vehicle.filePersistence.VehicleDataAccessStorage;
 import hu.lechnerkozpont.ambruspal.vehicle.interactor.VehicleInteractor;
-import hu.lechnerkozpont.ambruspal.vehicle.interactor.entity.Vehicle;
 import hu.lechnerkozpont.ambruspal.vehicle.interactor.interfaces.data.VehicleDataAccessInterface;
 import hu.lechnerkozpont.ambruspal.vehicle.ui.Controller;
 import hu.lechnerkozpont.ambruspal.vehicle.ui.Presenter;
 import hu.lechnerkozpont.ambruspal.vehicle.ui.View;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,13 +16,14 @@ public class Main {
         Controller controller = new Controller();
         Presenter presenter = new Presenter();
         View view = new View();
-        view.setPresenter(presenter);
-        view.setController(controller);
 
         vehicleInteractor.setVehicleDataAccessGateway(dataAccessStorage);
         vehicleInteractor.setVehicleDisplay(presenter);
         controller.setVehicleInteractor(vehicleInteractor);
         presenter.setVehicleInteractor(vehicleInteractor);
+        presenter.setView(view);
+        view.setPresenter(presenter);
+        view.setController(controller);
         view.run();
 
 
@@ -34,17 +32,5 @@ public class Main {
 //        Vehicle vehicle = presenter.viewVehicleByRegistrationNumber();
 //
 //        System.out.println("Vehicle.model :" + vehicle.getModel());
-
-
-
-
-
-//        Presenter presenter = new Presenter();
-//        VehicleInteractor vehicleInteractor = new VehicleInteractor();
-//        VehicleDataAccessStorage dataAccessStorage = new VehicleDataAccessStorage();
-//
-//        vehicleInteractor.setDataAccessInterface(dataAccessStorage);
-//        vehicleInteractor.setVehicleDisplayInterface(presenter);
-//        vehicleInteractor.findVehicleByRegistrationNumber("333");
     }
 }
